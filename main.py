2220f3fb2bfa8aa5b63a3004b53f513828e23990
Jöran Gläser <Joeran.glaeser@gmail.com>
2025-12-28 20:25:13 +0100

    Added Store class

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
-from Best_Buy.products import Product
+from Best_Buy import products, store
+from Best_Buy.store import Store
 
-bose = Product("Bose QuietComfort Earbuds", price=250, quantity=500)
-mac = Product("MacBook Air M2", price=1450, quantity=100)
+product_list = [products.Product("MacBook Air M2", price=1450, quantity=100),
+                products.Product("Bose QuietComfort Earbuds", price=250, quantity=500),
+                products.Product("Google Pixel 7", price=500, quantity=250),
+               ]
 
-print(bose.buy(50))
-print(mac.buy(100))
-print(mac.is_active())
-
-bose.show()
-mac.show()
-
-bose.set_quantity(1000)
-bose.buy(1000)
-bose.show()
+best_buy = Store(product_list)
+products = best_buy.get_all_products()
+print(best_buy.get_total_quantity())
+print(best_buy.order([(products[0], 1), (products[1], 2)]))
---
+++ store.py
@@ -0,0 +1,48 @@
+class Store:
+    def __init__(self, products=None):
+        if products is None:
+            products = []
+        try:
+            self.products = products
+        except ValueError:
+            print("Enter a valid Product!")
+
+    def add_product(self, product):
+        try:
+            self.products.append(product)
+        except ValueError or TypeError:
+            print("Enter a product to append to the list")
+
+    def remove_product(self, product):
+        try:
+            if product in self.products:
+                self.products.remove(product)
+        except ValueError or TypeError:
+            print("Product not found. Please enter a product from the .products list")
+
+    def get_total_quantity(self):
+        """ return the total quantity(int) of Products from a Store """
+        total_amount = 0
+        if not self.products:
+            return 0
+        for product in self.products:
+            total_amount += product.get_quantity()
+        return total_amount
+
+    def get_all_products(self):
+        """ return a list of Products from a Store """
+        if self.products:
+            return [product for product in self.products]
+        return []
+
+    def order(self, shopping_list):
+        total_price = 0.0
+        for product, quantity in shopping_list:
+            try:
+                if product in self.products:
+                    total_price += product.buy(quantity)
+                    if product.get_quantity() == 0:
+                        self.remove_product(product)
+            except TypeError or ValueError:
+                print(f"{product} could not be found in store.")
+        return total_price
